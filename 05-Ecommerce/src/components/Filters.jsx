1dab7ad17d9541d71bbc0a397c631a785424930a
Gabriel Funes <gabrielfunes100202@gmail.com>
2023-11-14 12:51:10 -0300

    Cambios en interfaz de filtros

--- 05-Ecommerce/src/components/Filters.jsx
+++ 05-Ecommerce/src/components/Filters.jsx
@@ -2,14 +2,14 @@ export const Filters = () => {
   return (
     <section className="flex w-1/5 flex-col items-center justify-start gap-12 bg-primary-50 px-5 py-20">
       <div className="flex flex-col items-center justify-center">
-        <span className="text-lg">Precio Máximo</span>
+        <span className="text-md">Precio Máximo</span>
         <input type="range" min="0" max="100000" step="100" />
-        <label htmlFor="price" className="text-lg">
+        <label htmlFor="price" className="text-md">
           $1000
         </label>
       </div>
       <div className="flex w-full flex-col items-center justify-center gap-1 px-12">
-        <span className="mb-5 text-lg">Filtros por categoría</span>
+        <span className="text-md mb-5 text-center text-lg">Categorías</span>
         <div className="flex w-full items-center justify-between">
           <label className="font-semibold" htmlFor="Mundiales">
             Mundiales
