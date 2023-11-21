c427956d4f43c49ee250f2fdb7485d4f36f96563
Gabriel Funes <gabrielfunes100202@gmail.com>
2023-11-21 12:53:57 -0300

    Mejorando contexto

--- 05-Ecommerce/src/context/cart.jsx
+++ 05-Ecommerce/src/context/cart.jsx
@@ -1,12 +1,19 @@
 import { createContext, useState } from "react";
 
-//crecion del contexto
+//creacion del contexto
 export const cartContext = createContext();
+
+//creacion del proveedor
 export const CartProvider = ({ children }) => {
+  //definicion del estado inicial y la funcion que lo actualiza
   const [cart, setCart] = useState([]);
+
+  //funciones que actualizan el estado del carrito
   const addToCart = (value) => {
     setCart(value);
   };
+
+  // retorno del proveedero del contexto creado con sus valor inicial y sus funciones correspondientes
   return (
     <cartContext.Provider value={{ cart, addToCart }}>
       {children}
