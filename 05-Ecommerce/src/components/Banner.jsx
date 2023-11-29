7bddb2be6ade8ebfd930abaf814a554fdd8a130c
Gabriel Funes <gabrielfunes100202@gmail.com>
2023-11-29 10:39:58 -0300

    Haciendo banner responsive

--- 05-Ecommerce/src/components/Banner.jsx
+++ 05-Ecommerce/src/components/Banner.jsx
@@ -13,18 +13,18 @@ export const Banner = () => {
           return (
             <div key={element.Url} className="relative w-full">
               <img
-                className="h-[500px] w-full object-cover"
+                className="h-[300px] sm:h-[500px] w-full object-cover"
                 src={element.Url}
                 alt="Imagen de cesped"
               />
-              <div className="absolute bottom-10 left-80 flex flex-col gap-5">
-                <h2 className="text-6xl font-semibold text-primary-50">
+              <div className="absolute bottom-12 sm:bottom-10 left-5 sm:left-20 md:left-40 lg:left-80 flex flex-col gap-5">
+                <h2 className="text-3xl sm:text-6xl font-semibold text-primary-50">
                   {element.Title}
                 </h2>
-                <h3 className="text-2xl font-semibold text-primary-50">
+                <h3 className="text-md sm:text-2xl font-semibold text-primary-50">
                   {element.Description}
                 </h3>
-                <Button className="w-36 bg-primary-600 text-primary-50">
+                <Button className="w-28 sm:w-36 bg-primary-600 text-primary-50">
                   <Link to="all" state={{ offers: true }}>
                     {element.ButtonText}
                   </Link>
