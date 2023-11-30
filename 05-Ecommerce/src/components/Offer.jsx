45a53a4d8375b2bfcde3948e55236bc3e1d14a7e
Gabriel Funes <gabrielfunes100202@gmail.com>
2023-11-30 19:59:58 -0300

    arreglando responsive para md

--- 05-Ecommerce/src/components/Offer.jsx
+++ 05-Ecommerce/src/components/Offer.jsx
@@ -17,7 +17,7 @@ export const Offer = () => {
           </span>
         </Link>
       </div>
-      <div className="grid w-full grid-cols-[repeat(auto-fit,_minmax(180px,_1fr))] items-center justify-center gap-10">
+      <div className="grid w-full grid-cols-[repeat(auto-fit,_minmax(190px,_1fr))] items-center justify-center gap-10">
         {products &&
           products.map((element, index) => {
             if (index <= 4) {
