830f216cee9167541adabfa2c1673b0f57686511
Gabriel100201 <gabrielfunes100202@gmail.com>
2024-06-24 12:18:46 -0300

    Cambio descripcion de proyectos

--- 04-Portfolio/src/components/ProjectCard.jsx
+++ 04-Portfolio/src/components/ProjectCard.jsx
@@ -1,6 +1,6 @@
 import { SOCIALS } from "../constants/socials";
 import { IconButton } from "./IconButton";
-
+import '../styles/scrol-bar.css'
 
 // eslint-disable-next-line react/prop-types
 export const ProjectCard = ({ title, text, img, urlProject, urlGh }) => {
@@ -16,17 +16,17 @@ export const ProjectCard = ({ title, text, img, urlProject, urlGh }) => {
           }}
           >
         </div>
-        <div className="absolute inset-0 flex flex-col justify-between p-7 text-white opacity-100">
-          <div className="flex flex-col justify-start">
+        <div className="absolute inset-0 flex flex-col justify-between overflow-y-auto p-7 text-white opacity-100 card-project">
+          <div className="flex flex-col justify-start w-full">
             <h3 className="text-3xl xl:text-4xl font-semibold text-center xl:text-start mb-2">{title}</h3>
-            <p className="text-md text-start h-full font-semibold opacity-100 xl:opacity-0 group-hover:opacity-100 mt-5 overflow-hidden max-h-[12em] transition-all" >{text}</p>
+            <p className="text-md text-start font-semibold opacity-100 xl:opacity-0 group-hover:opacity-100 mt-5 h-full transition-all" >{text}</p>
           </div>
         </div>
       </a>
-      <div className="absolute">
+      <div className="absolute bottom-3 right-3">
           {
             urlGh && (urlGh != "private") && (
-              <a href={urlGh} target="_blank" className="" rel="noreferrer">
+            <a href={urlGh} target="_blank" className="" rel="noreferrer">
                 <IconButton socialMedia={SOCIALS.GH}></IconButton>
               </a>
             )
